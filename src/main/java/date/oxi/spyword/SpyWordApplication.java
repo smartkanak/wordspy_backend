adde8a15d48b5f9c9cb21c03a89e33f102a2339a
hoener <hamza.oener@adesso-mobile.de>
2023-09-15 20:26:56 +0200

    initial

---
+++ src/main/java/date/oxi/spyword/SpyWordApplication.java
@@ -0,0 +1,13 @@
+package date.oxi.spyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpyWordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/date/oxi/spyword/SpyWordApplicationTests.java
@@ -0,0 +1,13 @@
+package date.oxi.spyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
